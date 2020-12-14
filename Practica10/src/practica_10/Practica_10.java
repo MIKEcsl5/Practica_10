b40a12b4af39a62235dea0988467b4a80d692faa
Daniel Rojas <57378034+EclipseDR3@users.noreply.github.com>
2020-12-14 12:29:19 -0600

    Adaptando Input a la clase principal
    Ya añadí la clase input al paquete de la practica e implementé la lectura a los ejercicios que lo necesitan.

--- Practica10/src/practica_10/Practica_10.java
+++ Practica10/src/practica_10/Practica_10.java
@@ -16,28 +16,41 @@ public class Practica_10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ACtividad 1*****************");
+        System.out.println("Actividad 1*****************");
         try{
-            String mensajes[] = {"Antonio", "javier", "Gabriel"};
+            KeyboardInput input = new KeyboardInput();
+            String mensajes[]= new String[3];
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Escriba un nombre:\n");
+                mensajes[i] = input.readString();
+            }
             for (int i=0; i<4; i++){
                 System.out.println(mensajes[i]);
             }
         }catch(ArrayIndexOutOfBoundsException aie){
             System.out.println("Error: apuntador fuera de rango");
         }
-        System.out.println("Actividad 2**************");
-        try{
-            float equis = 5/0;
         
+        
+        System.out.println("\nActividad 2**************");
+        try{
+            KeyboardInput input = new KeyboardInput();
+            float a, equis;
+            System.out.print("Escriba un numero:\n");
+            a = input.readFloat();
+            equis = 5/a;
+            System.out.println("La division es: "+equis+"\n");
         }catch(ArithmeticException ae){
             System.out.println("Error: division entre 0");
         }finally{
             System.out.println("A pesar de todo se ejecuta finally");
         }
-        System.out.println("actiividad 3****************");
+        
+        
+        System.out.println("\nActividad 3****************");
         try{
             float equis = 5/0;
-            System.out.println("equisss"+equis);
+            System.out.println("Equis: "+equis);
         }catch(ArrayIndexOutOfBoundsException aie){
             System.out.println("Error: division entre 0");
         }catch(ArithmeticException ae){
@@ -49,9 +62,16 @@ public class Practica_10 {
         }
         System.out.println("Fuera de try-cath");
         
-        System.out.println("Actividad 4*******");
+        
+        System.out.println("\nActividad 4*******");
         try{
-            int division = division(8,0);
+            KeyboardInput input = new KeyboardInput();
+            int a, b;
+            System.out.print("Escriba un numero entero para el numerador:\n");
+            a = input.readInteger();
+            System.out.print("Escriba un numero entero para el denominador:\n");
+            b = input.readInteger();
+            int division = division(a,b);
             System.out.println("Division = "+ division);
         }catch(ArithmeticException e){
             System.out.println("Excepcion aritmetica");
@@ -61,14 +81,21 @@ public class Practica_10 {
         System.out.println("Division = "+ division);
         */
        
-       System.out.println("Actividad 5*******");
+       
+       System.out.println("\nActividad 5*******");
         try{
-            int division = division2(8,0);
+            KeyboardInput input = new KeyboardInput();
+            int a, b;
+            System.out.print("Escriba un numero entero para el numerador:\n");
+            a = input.readInteger();
+            System.out.print("Escriba un numero entero para el denominador:\n");
+            b = input.readInteger();
+            int division = division2(a,b);
             System.out.println("Division = "+ division);
         }catch(ArithmeticException e){
             System.out.println("Excepcion aritmetica");
         } 
-
+        
     }
     
     public static int division(int a, int b) throws ArithmeticException{
