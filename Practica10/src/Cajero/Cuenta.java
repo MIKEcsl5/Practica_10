21948acb24e4428aaa882cef287fbccbe90c80db
MIKEcsl5 <soccerpoof013@gmail.com>
2020-12-15 02:30:39 -0600

    Documentación
    Prueba para resolver error en la pagina de GithubPages

--- Practica10/src/Cajero/Cuenta.java
+++ Practica10/src/Cajero/Cuenta.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cajero;
+package Cajero;
 
 /**
  * Clase Cuenta del package cajero
--- Practica10/src/Cajero/SaldoInsuficienteException.java
+++ Practica10/src/Cajero/SaldoInsuficienteException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cajero;
+package Cajero;
 
 /**
  * Clase Saldo Insuficiente que hereda de la clase Exception
